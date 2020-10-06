2de98d32d4ea3fc6b0364001022569c77c26bc54
Alex Li <zijianli16@gmail.com>
2020-10-06 16:52:33 -0700

    Adding module

--- js/babylonBasicTemplate.js
+++ js/babylonBasicTemplate.js
@@ -1,6 +1,8 @@
 //import type auto completion file
 ///<reference path='babylon.d.ts' />
 
+import { startScrollama } from './scrollama.js';
+
 //-------------Testing get mouse/touch position
 var scrollytellingArea = document.getElementById("babylonScrollytelling");
 
@@ -282,7 +284,7 @@ function animateUniversalCamera(scene) {
     //universalCameraAni01.pause();
 
     //start scrollama
-    startScrollama();
+    startScrollama(scene, universalCamera);
 }
 //*************************ABOVES ARE CAMERAS ANIMATION*******************/
 
@@ -384,97 +386,4 @@ function debug(scene) {
     //Debug: show Scene Explore and Inspector
     scene.debugLayer.show();
 }
-
-//==========Adding scrollama.js below==================
-function startScrollama() {
-
-    const scrollamaScene = scrollama();
-    // setup the instance, pass callback functions
-
-    scrollamaScene
-        .setup({
-            step: ".step",
-            //debug: true,
-            offset: 0
-        })
-
-        .onStepEnter(response => {
-            // { element, index, direction }
-
-            function changeCameraPosition01() {
-
-                let newIndex = response.index;
-                let direction = response.direction;
-
-                if (newIndex == 0 && direction == 'down') {
-                    scene.beginAnimation(universalCamera, 0, 30, true, 0.2);
-                }
-                else if (newIndex == 0 && direction == 'up') {
-                    scene.beginAnimation(universalCamera, 30, 0, true, 0.2);
-                }
-                else if (newIndex == 1 && direction == "down") {
-                    scene.beginAnimation(universalCamera, 30, 60, true, 0.2);
-                }
-                else if (newIndex == 1 && direction == "up") {
-                    scene.beginAnimation(universalCamera, 60, 30, true, 0.2);
-                }
-                else if (newIndex == 2 && direction == "down") {
-                    scene.beginAnimation(universalCamera, 60, 90, true, 0.2);
-                }
-                else if (newIndex == 2 && direction == "up") {
-                    scene.beginAnimation(universalCamera, 90, 60, true, 0.2);
-                }
-            }
-
-            changeCameraPosition01();
-
-            console.log("Entered");
-            console.log(response.index);
-            console.log(response.direction);
-
-        })
-
-        .onStepExit(response => {
-            // { element, index, direction }
-            function changeCameraPosition02() {
-
-                let newIndex = response.index;
-                let direction = response.direction;
-
-                if (newIndex == 0 && direction == "down") {
-                    // scene.beginAnimation(universalCamera, 0, 30, true, 0.2);
-                }
-                else if (newIndex == 0 && direction == "up") {
-                    // scene.beginAnimation(universalCamera, 0, 30, true, 0.2);
-
-                }
-                else if (newIndex == 1 && direction == "down") {
-
-                }
-                else if (newIndex == 1 && direction == "up") {
-
-                }
-                else if (newIndex == 2 && direction == "down") {
-
-                }
-                else if (newIndex == 2 && direction == "up") {
-
-                }
-            }
-
-            changeCameraPosition02();
-
-            console.log("Exit");
-            console.log(response.index);
-            console.log(response.direction);
-
-        })
-
-    // setup resize event
-    window.addEventListener("resize", scrollamaScene.resize);
-    //==========Ending of scrollama.js==================
-
-}
-
-
 
